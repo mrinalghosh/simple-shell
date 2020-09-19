2205557f2c650d0a0a8e06ed7c4eec9c75eacd9a
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-19 13:02:43 -0400

    strtok test

--- myshell.c
+++ myshell.c
@@ -1,5 +1,13 @@
 #include "myshell.h"
 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 #define TRUE 1
 #define MAX_TOKEN 1 << 5
 #define MAX_BUFFER 1 << 9
@@ -18,21 +26,31 @@ void type_prompt(void) {
 int main(int argc, char** argv) {
     char* buffer = (char*)malloc(sizeof(char) * MAX_BUFFER);  //store line
     char* command = (char*)malloc(sizeof(char) * MAX_TOKEN);  //store command token
-    char* token = (char*)malloc(sizeof(char) * MAX_TOKEN);  // TODO: does this need to be smaller?
+    char* token = (char*)malloc(sizeof(char) * MAX_TOKEN);    // TODO: does this need to be smaller?
 
     pid_t pid;
+    int wstatus;
 
     while (TRUE) {
         type_prompt();
         fgets(buffer, MAX_BUFFER, stdin);
-        printf("%s\n", buffer);
 
         // tokenize buffer - should eventually be in read_command(command, parameters);
         token = strtok(buffer, " ");
         while (token != NULL) {
             printf(" %s\n", token);
-
             token = strtok(NULL, " ");
+        }
+
+        pid = fork();
+        if (pid == 0) {
+            // child
+            printf("Hello from child\n");
+        } else if (pid > 0) {
+            // parent
+            printf("Hello from parent..waiting\n");
+            pid = waitpid(pid, &wstatus, 0);
+            printf("child exited with status %d", WEXITSTATUS(wstatus));
         }
 
         // if ((pid = fork()) > 0) {  // might not need to validate - fork arguments?
--- myshell.h
+++ myshell.h
@@ -1,11 +1,6 @@
 #ifndef MYSHELL_H
 #define MYSHELL_H
 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-
 void type_prompt(void);
 // void read_command(char* command, char* parameters);
 void error(char* message);
