76512a855cddb98fbb3dc4370a688d8fbae062ee
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-19 12:22:04 -0400

    fork test

--- myshell.c
+++ myshell.c
@@ -8,28 +8,32 @@ void type_prompt(void) {
     printf("my_shell$: ")
 }
 
-static bool validate(char* token){
+// static bool validate(char* token){
 
-}
+// }
 
-void error_message(void) {
-}
+// void error_message(void) {
+// }
 
 int main(int argc, char** argv) {
     char* buffer = malloc(sizeof(char) * MAX_BUFFER);  //store line
     char* token = malloc(sizeof(char) * MAX_TOKEN);    //store command token
 
+    pid_t pid;
+
     while (TRUE) {
         type_prompt();
         fgets(buffer, MAX_BUFFER, stdin);
         // read_command(command, parameters);
-        if (validate(token) & !fork()) {  // fork arguments?
+        if ((pid = fork()) > 0) {  // might not need to validate - fork arguments?
             // parent
             // waitpid(-1, &status, 0);
+            printf("hello parent\n");
         } else {
             // child
             // execvp(command, parameters, 0);  // execvp doesn't need absolute path
             //need to hardcode metachars & > < |
+            printf("hello child\n");
         }
     }
 }
--- myshell.h
+++ myshell.h
@@ -5,22 +5,8 @@
 #include <string.h>
 #include <unistd.h>
 
-/* NOT NEEDED SINCE ALL BINARY EXE NEED TO BE SUPPORTED
-typedef struct {
-    char* path;
-    int argc;
-} binexe;
-
-enum commands {
-    CAT,
-    LS,
-    PS,
-    COMMAND_COUNT
-};
-*/
-
 void type_prompt(void);
-void read_command(char* command, char* parameters);
+// void read_command(char* command, char* parameters);
 void error(char* message);
 
 #endif /*MYSHELL_H*/
