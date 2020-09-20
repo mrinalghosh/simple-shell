afc32fbd66e62183dd1871869e0815a8eb69b80a
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-20 15:44:28 -0400

    cmd handling

--- myshell.c
+++ myshell.c
@@ -76,9 +76,11 @@ int commandParser(char* tokens[]) {
     char* metachars[4] = {">", "<", "|", "&"};
     bool metamask[4] = {false, false, false, false};
 
-    char* base_tokens[TOKEN_LIMIT];   // to hold left side arguments - this won't work with more than 1 meta-char - might need more?
+    char* tokens_cp[TOKEN_LIMIT];     // to hold left side arguments - this won't work with more than 1 meta-char - might need more?
     char* clean_tokens[TOKEN_LIMIT];  // right side of meta-delimited args
-    char* subtoken;
+    char* subtokens[TOKEN_LIMIT];     // doesn't need to be this large
+
+    size_t subcount = 0;
 
     size_t i = 0, j = 0, k = 0;
 
@@ -86,26 +88,35 @@ int commandParser(char* tokens[]) {
         for (k = 0; k < 4; ++k)
             if (strcmp(tokens[i], metachars[k]) == 0) metamask[j] = true;
 
-        if (metamask[0] || metamask[1] || metamask[2] || metamask[3])
-            break;
+        // if (metamask[0] || metamask[1] || metamask[2] || metamask[3])
+        //     break; // JUST CHECKING FOR FIRST MASK
 
-        if ((subtoken = strstr(tokens[i], metachars[2])) == NULL)  // not a metachar in string
+        // if ((subtokens = strstr(tokens[i], metachars[2])) == NULL)  // not a metachar in string
+        //     clean_tokens[j] = tokens[i];
+        // else {
+        //     clean_tokens[j] = tokens[i];  //TODO: only print difference between string and subtokens
+        //     clean_tokens[++j] = metachars[2];
+        //     clean_tokens[++j] = subtokens;
+        // }
+
+        // break up tokens wiht meta
+        if (strchr(tokens[i], '|') == NULL)  // no pipe - just add - change to support all meta
             clean_tokens[j] = tokens[i];
         else {
-            clean_tokens[j] = tokens[i];  //TODO: only print difference between string and subtoken
-            clean_tokens[++j] = metachars[2];
-            clean_tokens[++j] = subtoken;
+            while (subtokens[subcount] = strtok(NULL, "|&<>") != NULL) {
+                ++subcount;
+            }
         }
 
         base_tokens[i] = tokens[i];
         ++i;
-        ++j;
+        // ++j;
     }
 
     //TODO: BREAK UP FOR METACHARS WO SPACES -> DELIMITED
 
-    for (k = 0; k < j; ++k) {
-        printf("%s--", clean_tokens[k]);
+    for (k = 0; k < subcount; ++k) {
+        printf("--%s--\n", clean_tokens[k]);
     }
 
     if (metamask[2]) {        // PIPE
@@ -118,9 +129,11 @@ int commandParser(char* tokens[]) {
 
 int main(int argc, char** argv) {
     int token_count;
-    int status;
+
     int suppress = (argc > 1) && !strcmp(argv[1], "-n");  // suppress output
+
     pid_t pid;
+    int status;
     int retcode;
 
     char buffer[MAX_BUFFER];    // DON'T NEED TO MALLOC THESE - MAX SIZE GIVEN
