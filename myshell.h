5ad324af38473069e4a5f06549ba1abe226522d0
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-20 21:30:45 -0400

    metachar struct segfaulting

--- myshell.h
+++ myshell.h
@@ -1,9 +1,9 @@
 #ifndef MYSHELL_H
 #define MYSHELL_H
 
-#define MAX_TOKEN 1ULL << 5    // maximum token size
-#define MAX_BUFFER 1ULL << 9   // maximum buffer size
-#define TOKEN_LIMIT 1ULL << 7  // maximum number of tokens - arbitrary choice of 128
+#define MAX_TOKEN (1ULL << 5)    // maximum token size
+#define MAX_BUFFER (1ULL << 9)   // maximum buffer size
+#define TOKEN_LIMIT (1ULL << 7)  // maximum number of tokens - arbitrary choice of 128
 #define STD_INPUT 0
 #define STD_OUTPUT 1
 #define STD_ERROR 2
