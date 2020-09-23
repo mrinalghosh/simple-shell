35d8d3ba53d8396bf93265b7cbcd1cbd3425d626
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-22 20:02:09 -0400

    single cmd fix

--- myshell.c
+++ myshell.c
@@ -246,57 +246,53 @@ void command_handler(char* tokens[]) {
     i = 0;  // row counter (even->A, odd->M)
     j = 0;  // metacharacter counter
 
-    else {
-        while (token_array[i][0] != NULL) {  // loop over rows of token_array and act at every metacharacter
+    while (token_array[i][0] != NULL) {  // loop over rows of token_array and act at every metacharacter
 
-            if (strcomp(token_array[i][0], "|<>", 3) != 0) {
-                ++i;
-                continue;
-            }
-
-            if ((pid = fork()) == -1) {
-                perror("fork failed");
-                exit(1);
-
-            } else if (pid > 0) {
-                /* Parent */
-                if (!bg)
-                    pid = waitpid(pid, &status, 0);
-                else
-                    signal(SIGCHLD, child_handler);
-
-            } else {
-                /* Child */
-                if (row == 1) {
-                    if (execvp(token_array[0][0], token_array[0]) < 0) {
-                        perror("execvp failed");
-                        exit(0);
-                    }
-                }
-
-                if (strcmp(token_array[i][0], "<") == 0) {
-                    ffd = open(token_array[i + 1][0], rflags);  // assuming only one file can be redirected
-                    dup2(ffd, STDIN_FILENO);
-
-                    execvp(token_array[i - 1][0], token_array[i - 1]);
-
-                    close(ffd);
-                    exit(0);
-                }
-
-                if (strcmp(token_array[i][0], ">") == 0) {
-                    ffd = open(token_array[i + 1][0], wflags);
-                    dup2(ffd, STDOUT_FILENO);
-
-                    execvp(token_array[i - 1][0], token_array[i - 1]);
-
-                    close(ffd);
-                    exit(0);
-                }
-            }
-
+        if (strcomp(token_array[i][0], "|<>", 3) != 0) {
             ++i;
+            continue;
         }
+
+        if ((pid = fork()) == -1) {
+            perror("fork failed");
+            exit(1);
+
+        } else if (pid > 0) {
+            /* ---Parent--- */
+            if (!bg)
+                pid = waitpid(pid, &status, 0);
+            else
+                signal(SIGCHLD, child_handler);
+
+        } else {
+            /* ---Child--- */
+            if (row == 1 && (execvp(token_array[0][0], token_array[0]) < 0)) {
+                perror("execvp failed");
+                exit(0);
+            }
+
+            if (strcmp(token_array[i][0], "<") == 0) {
+                ffd = open(token_array[i + 1][0], rflags);  // assuming only one file can be redirected
+                dup2(ffd, STDIN_FILENO);
+
+                execvp(token_array[i - 1][0], token_array[i - 1]);
+
+                close(ffd);
+                exit(0);
+            }
+
+            if (strcmp(token_array[i][0], ">") == 0) {
+                ffd = open(token_array[i + 1][0], wflags);
+                dup2(ffd, STDOUT_FILENO);
+
+                execvp(token_array[i - 1][0], token_array[i - 1]);
+
+                close(ffd);
+                exit(0);
+            }
+        }
+
+        ++i;
     }
 
     return;
