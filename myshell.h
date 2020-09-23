0a41f71c372060073f7e56100b0b1851d73bf8b1
Mrinal Ghosh <ghoshm@bu.edu>
2020-09-23 06:02:57 -0400

    0602

--- myshell.h
+++ myshell.h
@@ -4,14 +4,14 @@
 #define MAX_TOKEN (1ULL << 5)    // maximum token size
 #define MAX_BUFFER (1ULL << 9)   // maximum buffer size
 #define TOKEN_LIMIT (1ULL << 7)  // maximum number of tokens - arbitrary choice of 128
-#define MAX_FILE (1ULL << 12)    // 4k file buffer size
 #define STD_INPUT 0
 #define STD_OUTPUT 1
 #define STD_ERROR 2
-#define WRITE 0  // pipe fd[0] corresponds to write to buffer stdin
-#define READ 1   // pipe fd[1] corresponds to read from buffer stdout
 
-void type_prompt(void);
-void error(char* message);
+void prompt(void);
+bool chcmp(char ch, char* list, int n);
+bool strsearch(char* str, char* list, int n);
+void nanny(int signum);
+void command_handler(char* tokens[]);
 
 #endif /*MYSHELL_H*/
